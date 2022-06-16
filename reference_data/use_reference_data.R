2797d562edccf3ff83d384a216b0c2fbbe0a5596
lehmkudc <lehmkudc@gmail.com>
2022-06-15 22:27:29 -0400

    set up new new experimental data to test

--- reference_data/use_reference_data.R
+++ reference_data/use_reference_data.R
@@ -57,3 +57,78 @@ aspenData %>%
   ) %>%
   select( exp, everything() ) %>%
   write.csv("./aspen_comparison/aspen_comparison.csv",row.names = FALSE)
+
+
+
+
+
+table_pieces <- readLines("reference_data/mondejar_et_al_raw.txt",encoding="Latin-1") %>%
+  strsplit( "SPACE") %>% unlist() %>% map( function(x){
+    table_piece <- strsplit( x, " ") %>% 
+      unlist() %>%
+      .[. != "ï»¿"] %>%
+      .[. != ""] %>%  
+      str_replace(string = ., pattern = "âˆ’", replacement = "-") %>% 
+      as.numeric() %>%
+      matrix(., ncol = 5,byrow = TRUE) %>%
+      as.data.frame()
+    
+    colnames( table_piece ) <- c(
+      "Tk", "P", "density", "rho_diff", "uncertainty"
+    )
+    
+    table_piece <- table_piece %>%
+      select( Tk, P, density )
+    
+    return(
+      table_piece 
+    )
+  })
+
+bind_rows(
+  table_pieces[[1]] %>% mutate(yco = 0.2),
+  table_pieces[[2]] %>% mutate(yco = 0.2),
+  table_pieces[[3]] %>% mutate(yco = 0.2),
+  table_pieces[[4]] %>% mutate(yco = 0.2),
+  table_pieces[[5]] %>% mutate(yco = 0.4),
+  table_pieces[[6]] %>% mutate(yco = 0.4),
+  table_pieces[[7]] %>% mutate(yco = 0.6),
+  table_pieces[[8]] %>% mutate(yco = 0.6),
+) %>%
+  mutate(
+    t_c = Tk - 273.15, #[*C]
+    p_bar = P*10, #[bar]
+    mfrac_co = ( 44.01*yco )/( 44.01*yco + 16.04*(1-yco)), 
+    rhoco = mfrac_co*density/44.01, #[mol/L]
+    rhome = (1-mfrac_co)*density/16.04
+  )
+
+
+
+bind_rows(
+  table_pieces[[1]] %>% mutate(yco = 0.2),
+  table_pieces[[2]] %>% mutate(yco = 0.2),
+  table_pieces[[3]] %>% mutate(yco = 0.2),
+  table_pieces[[4]] %>% mutate(yco = 0.2),
+  table_pieces[[5]] %>% mutate(yco = 0.4),
+  table_pieces[[6]] %>% mutate(yco = 0.4),
+  table_pieces[[7]] %>% mutate(yco = 0.6),
+  table_pieces[[8]] %>% mutate(yco = 0.6),
+) %>% 
+  mutate( 
+    exp = 1:n(),
+    t_c = Tk - 273.15,
+    p_bar = P*10,
+    s_box = 34,
+    n_moves = 1000,
+    n_equil = as.integer(1000000),
+    n_prod  = as.integer(100000)
+  ) %>%
+  select( exp, yco, p_bar, t_c, s_box, n_moves, n_equil, n_prod ) %>%
+  write_csv( "mixture_grid/mondejar_et_al.csv")
+
+
+
+
+
+
